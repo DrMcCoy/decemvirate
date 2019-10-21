dbc17d51e39a98611665db8565e3bb8d5314ec97
Sven Hesse <drmccoy@drmccoy.de>
2019-10-21 16:10:44 +0200

    MAIN: Add a stubby main()

---
+++ src/decemvirate.cpp
@@ -0,0 +1,27 @@
+/* Decemvirate - A FLOSS Pathfinder TTRPG helper
+ *
+ * Decemvirate is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * Decemvirate is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * Decemvirate is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with Decemvirate. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file
+ *  The project's main entry point.
+ */
+
+int main(void) {
+	return 0;
+}
